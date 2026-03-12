980f3d4ff23b27dc01cb64a2005aaa8fcfa494b7
Matheus de Farias <matheusfariasshalom@gmail.com>
2026-03-12 13:27:17 -0300

    Criar meta tags para SEO

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,48 +2,47 @@ import { Postpage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
-import { title } from "process";
 
-type PostPageProps ={
+type PostPageProps = {
     params: Promise<{
-        slug:string
+        slug: string
     }>
 }
 
-export async function generateMetaData({params}:PostPageProps):Promise<Metadata> {
-    const {slug} = await params;
+export async function generateMetadata({ params }: PostPageProps): Promise<Metadata> {
+    const { slug } = await params;
     const post = allPosts.find((post) => post.slug === slug);
 
-    if(!post){
-        return{}
+    if (!post) {
+        return {}
     }
 
-    return{
-        title:post.title,
-        description:post.description,
-        authors:[{name:post.author.name}],
-        robots:'index,follow',
-        openGraph:{
-            images:[post.image]
+    return {
+        title: post.title,
+        description: post.description,
+        authors: [{ name: post.author.name }],
+        robots: 'index,follow',
+        openGraph: {
+            images: [post.image]
         }
     }
 }
 
 export async function generateStatisParams() {
-    return allPosts.map((post) =>({
-        slug:post.slug
+    return allPosts.map((post) => ({
+        slug: post.slug
     }))
 }
 
-export default async function BlogPostPage({params}:PostPageProps){
-   const {slug} = await params;
-   const post = allPosts.find((post) => post.slug === slug)
+export default async function BlogPostPage({ params }: PostPageProps) {
+    const { slug } = await params;
+    const post = allPosts.find((post) => post.slug === slug)
 
-if(!post){
-    notFound();
-}
+    if (!post) {
+        notFound();
+    }
 
-    return(
+    return (
         <Postpage post={post} />
     )
 }
