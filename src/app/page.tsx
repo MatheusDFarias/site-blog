1b3ea930bd81fcc5f9f85401459156409373796d
Matheus de Farias <matheusfariasshalom@gmail.com>
2026-03-12 13:03:48 -0300

    Criar meta tags para SEO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,25 @@
 import { LandingPage } from "@/templates/landing-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata ={
+    title:'Site-set',
+    description:'Venda seus produto como afiliado em um único lugar',
+    robots:'index,follow',
+    openGraph:{
+        title:'Site-set',
+        description:'Venda seus produto como afiliado em um único lugar',
+        url:'https://site-blog-khaki-gamma.vercel.app/og-image.png',
+        siteName:'Site-set',
+        locale:'pt_BR',
+        type:'website',
+        images:{
+            url:'https://site-blog-khaki-gamma.vercel.app/og-image.png',
+            width:800,
+            height: 600,
+            alt:'Site-set'
+        }
+    }
+}
 
 export default function HomePage(){
     return(
