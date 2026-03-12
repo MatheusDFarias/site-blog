6ced9a4947adacc512add70050589ef08af3e949
Matheus de Farias <matheusfariasshalom@gmail.com>
2026-03-12 13:25:34 -0300

    Criar meta tags para SEO

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,11 +1,38 @@
 import { Postpage } from "@/templates/blog";
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
+import { title } from "process";
 
 type PostPageProps ={
     params: Promise<{
         slug:string
     }>
+}
+
+export async function generateMetaData({params}:PostPageProps):Promise<Metadata> {
+    const {slug} = await params;
+    const post = allPosts.find((post) => post.slug === slug);
+
+    if(!post){
+        return{}
+    }
+
+    return{
+        title:post.title,
+        description:post.description,
+        authors:[{name:post.author.name}],
+        robots:'index,follow',
+        openGraph:{
+            images:[post.image]
+        }
+    }
+}
+
+export async function generateStatisParams() {
+    return allPosts.map((post) =>({
+        slug:post.slug
+    }))
 }
 
 export default async function BlogPostPage({params}:PostPageProps){
